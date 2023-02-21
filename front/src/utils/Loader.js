a933e6c3fa61d3e924a1b6e48d2a1017ac3d4738
Luis Ruiz <luis.ruiz@globalalumni.org>
2023-02-21 15:45:13 +0100

    first commit

--- front/src/utils/Loader.js
+++
@@ -1,9 +0,0 @@
-
-function Loader({ type }){
-    return(
-        <div className={`loaderPadre ${type}`}>
-            <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> Cargando...
-        </div>
-    )
-}
-export default Loader
